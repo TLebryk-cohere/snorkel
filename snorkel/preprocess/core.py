83edda8cbfa1337a3ab441ea1d82c1478c048845
TLebryk-cohere <theo.lebryk@coherehealth.com>
2022-10-09 11:24:59 -0400

    add medspacy preprocess and non-pickle preprocessing

--- snorkel/preprocess/core.py
+++ snorkel/preprocess/core.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from snorkel.map import BaseMapper, LambdaMapper, Mapper, lambda_mapper
+from snorkel.types import DataPoint
 
 """Base classes for preprocessors.
 
@@ -59,4 +62,42 @@ class preprocessor(lambda_mapper):
     ...     return NEGATIVE
     """
 
-    pass
+    def __call__(self, x: DataPoint) -> Optional[DataPoint]:
+        """Run mapping function on input data point.
+
+        Deep copies the data point first so as not to make
+        accidental in-place changes. If ``memoize`` is set to
+        ``True``, an internal cache is checked for results. If
+        no cached results are found, the computed results are
+        added to the cache.
+
+        Parameters
+        ----------
+        x
+            Data point to run mapping function on
+
+        Returns
+        -------
+        DataPoint
+            Mapped data point of same format but possibly different fields
+        """
+        if self.memoize:
+            # NB: don't do ``self._cache.get(...)`` first in case cached value is ``None``
+            x_hashable = self._memoize_key(x)
+            if x_hashable in self._cache:
+                return self._cache[x_hashable]
+        # Dangerous; avoids pickling but copy() 
+        # isn't perfect in avoiding mutation on original object.
+        x_mapped = x.copy()
+        # x_mapped = pickle.loads(pickle.dumps(x))
+        for mapper in self._pre:
+            x_mapped = mapper(x_mapped)
+        x_mapped = self._generate_mapped_data_point(x_mapped)
+        if self.memoize:
+            self._cache[x_hashable] = x_mapped
+        return x_mapped
+
+    def __repr__(self) -> str:
+        pre_str = f", Pre: {self._pre}"
+        return f"{type(self).__name__} {self.name}{pre_str}"
+
